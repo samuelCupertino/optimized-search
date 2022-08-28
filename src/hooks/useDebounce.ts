235e5cfa2fbbd66cf06aeef046a70019f05391ca
Samuel Cupertino <sacupertinno2014@gmail.com>
2022-08-28 19:04:51 -0300

    ajuste na tipagem do useDebounce

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,11 +1,6 @@
 import { useState, useEffect } from 'react'
 
-interface IUseDebounceData<T> {
-  value: T
-  isWaiting: boolean
-}
-
-export const useDebounce = <T>(value: T, delay = 500): IUseDebounceData<T> => {
+export const useDebounce = <T>(value: T, delay = 500) => {
   const [debouncedValue, setDebouncedValue] = useState(value)
 
   useEffect(() => {
